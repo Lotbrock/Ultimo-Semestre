a57e8e4212c31676ec93775d8599af17e4827395
Julian Fajardo <jafajardo37@misena.edu.co>
2019-03-20 12:52:57 -0500

    chore: Se crea la entidad EPS

---
+++ eps/src/main/java/co/edu/sena/eps/entities/Eps.java
@@ -0,0 +1,38 @@
+package co.edu.sena.eps.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Eps", uniqueConstraints =@UniqueConstraint(name = "Unique_nit",columnNames = {"nit_eps"}))
+public class Eps {
+    @Id
+    private int id;
+    @Column(name = "nombre_eps", nullable = false)
+    private String nombreEPS;
+    @Column(name = "nit_eps", nullable = false)
+    private String nitEPS;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreEPS() {
+        return nombreEPS;
+    }
+
+    public void setNombreEPS(String nombreEPS) {
+        this.nombreEPS = nombreEPS;
+    }
+
+    public String getNitEPS() {
+        return nitEPS;
+    }
+
+    public void setNitEPS(String nitEPS) {
+        this.nitEPS = nitEPS;
+    }
+}
