954908b5dabb49fee94564fd383fff49e1bf7dca
Julian Fajardo <jafajardo37@misena.edu.co>
2019-03-20 15:27:13 -0500

    Chore: Se crean las entidades Cargo y Funcionario

---
+++ eps/src/main/java/co/edu/sena/eps/entities/Cargo.java
@@ -0,0 +1,41 @@
+package co.edu.sena.eps.entities;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name="Cargo",
+        uniqueConstraints=@UniqueConstraint(columnNames={"nombre_cargo"}))
+public class Cargo {
+    @Id
+    private Long id;
+    @Column(name = "nombre_cargo", nullable = false)
+    private String nombreCargo;
+    @ManyToMany(mappedBy = "cargos")
+    List<Funcionario> funcionarios;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombreCargo() {
+        return nombreCargo;
+    }
+
+    public void setNombreCargo(String nombreCargo) {
+        this.nombreCargo = nombreCargo;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+}
---
+++ eps/src/main/java/co/edu/sena/eps/entities/Funcionario.java
@@ -0,0 +1,48 @@
+package co.edu.sena.eps.entities;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "Funcionario")
+public class Funcionario {
+    @Id
+    private Long id;
+    @OneToOne
+    private Usuario idUsuario;
+
+    @ManyToMany(cascade = {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
+    @JoinTable(name = "funcionarios_has_cargo",
+            joinColumns = @JoinColumn(name = "funcionario_id"),
+            inverseJoinColumns = @JoinColumn(name = "cargo_id")
+    )
+    private List<Cargo> cargos;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Usuario idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public List<Cargo> getCargos() {
+        return cargos;
+    }
+
+    public void setCargos(List<Cargo> cargos) {
+        this.cargos = cargos;
+    }
+}
